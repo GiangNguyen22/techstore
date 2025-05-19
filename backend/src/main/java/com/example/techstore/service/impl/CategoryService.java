4ecb6797c063018f68e995c67af79a25d16c20a2
Dean Nguyen <nguyenvangiang2212jr@gmail.com>
2025-05-19 15:41:23 +0700

    add category

--- backend/src/main/java/com/example/techstore/service/impl/CategoryService.java
+++ backend/src/main/java/com/example/techstore/service/impl/CategoryService.java
@@ -1,9 +1,13 @@
 package com.example.techstore.service.impl;
 
 import com.example.techstore.entity.Category;
+import com.example.techstore.exceptions.ResourceNotFoundEx;
 import com.example.techstore.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CategoryService  {
@@ -13,5 +17,28 @@ public class CategoryService  {
 
     public Category getCategoryById(int id) {
         return categoryRepository.findById(id).orElse(null);
+    }
+
+
+    public List<Category> getAllCategory() {
+        return categoryRepository.findAll();
+    }
+
+    public Category createCategory(Category category) {
+        category.setId(null);
+        return categoryRepository.save(category);
+    }
+
+    public Category updateCategory(Category category, Integer categoryId) {
+        Category category1 = categoryRepository.findById(categoryId).orElseThrow(()-> new ResourceNotFoundEx("Category not found with id " + categoryId));
+        if(category.getName() != null) {
+            category1.setName(category.getName());
+        }
+        return categoryRepository.save(category1);
+    }
+
+    public void deleteCategory(Integer categoryId) {
+        Category category = categoryRepository.findById(categoryId).orElseThrow(()-> new ResourceNotFoundEx("Category not found with id " + categoryId)) ;
+        categoryRepository.deleteById(categoryId);
     }
 }
